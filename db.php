a7fa3057644c51a29ddef5593cdc967fd8ab1e6e
OblakZan <jstsmjst0101@gmail.com>
2019-05-19 18:39:18 +0200

    Trgovko v Primerjalko

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database_name = "Trgovko";
+$database_name = "Primerjalko";
 
 $conn = new mysqli($servername, $username, $password, $database_name);
 
