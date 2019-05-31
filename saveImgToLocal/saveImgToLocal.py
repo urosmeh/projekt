7c34c102b95bc716cf2b5ce3c12e00fbddb5722c
urosm_221sxwk <uros.meh@gmail.com>
2019-05-31 14:47:02 +0200

    get all img urls

--- saveImgToLocal/saveImgToLocal.py
+++ saveImgToLocal/saveImgToLocal.py
@@ -2,9 +2,10 @@ import pymysql
 
 conn = pymysql.connect(host='localhost', user='root', password='', db='primerjalko')
 db = conn.cursor()
-sql = "select * from products;"
+sql = "select url from pictures;"
 
 countrows = db.execute(sql)
 print("num of rows: ", countrows)
-data = db.fetchone()
-print(data)
+data = db.fetchall()
+for row in data:
+    print(row[0])
