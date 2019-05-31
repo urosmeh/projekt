3bea95f80fccb46d8cee56ff1e18b704fe49dad0
urosm_221sxwk <uros.meh@gmail.com>
2019-05-31 23:14:19 +0200

    testing connectivity with database

--- saveImgToLocal/saveImgToLocal.py
+++ saveImgToLocal/saveImgToLocal.py
@@ -1,11 +1,15 @@
 import pymysql
+import os
+import requests
 
 conn = pymysql.connect(host='localhost', user='root', password='', db='primerjalko')
 db = conn.cursor()
 sql = "select url from pictures;"
+db.execute(sql)
 
 countrows = db.execute(sql)
 print("num of rows: ", countrows)
+
 data = db.fetchall()
 for row in data:
     print(row[0])
