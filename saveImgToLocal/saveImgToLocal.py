6da0c71ee638b16a9543a244b92625a01dd96b97
urosm_221sxwk <uros.meh@gmail.com>
2019-05-31 23:30:36 +0200

    added functionality to download images to machine

--- saveImgToLocal/saveImgToLocal.py
+++ saveImgToLocal/saveImgToLocal.py
@@ -7,9 +7,13 @@ db = conn.cursor()
 sql = "select url from pictures;"
 db.execute(sql)
 
-countrows = db.execute(sql)
-print("num of rows: ", countrows)
+#countrows = db.execute(sql)
+#print("num of rows: ", countrows)
 
 data = db.fetchall()
 for row in data:
-    print(row[0])
+    #print(row[0])
+    url = row[0]
+    filename = url.split('/')[-1]
+    r = requests.get(url, allow_redirects=True)
+    open(filename, 'wb').write(r.content)
