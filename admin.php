11af398804e8cf6fe50623fab240d9ae7a91ee96
urosm_221sxwk <uros.meh@gmail.com>
2019-06-02 00:19:23 +0200

    fixed scraper link text

--- admin.php
+++ admin.php
@@ -12,7 +12,7 @@ if(isset($_SESSION['Name']) && isset($_SESSION['Surname'])&& $_SESSION['Admin']=
     echo "<td><a href='scraper_bigbang.php'> BigBang Scraper </a></td>";
     echo "<td><a href='mlacom.php'> Mlacom Scraper </a></td>";
     echo "<td><a href='markosoftscrap.php'> Markosoft Scraper </a></td>";
-    echo "<td><a href='markosoftscrap.php'> Primerjalko Scraper </a></td>";
+    echo "<td><a href='scraper_primerjalko.php'> Pričaraj Scraper </a></td>";
 
     echo"</tr><tr>";
 
