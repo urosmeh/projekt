997255c1ab8718e7ac02c7b134843dfe82e46f4a
OblakZan <jstsmjst0101@gmail.com>
2019-06-02 14:19:13 +0200

    Comparison.py inserts comparison data into db.

--- comparison.py
+++ comparison.py
@@ -6,7 +6,9 @@ import skimage
 from skimage import exposure
 from skimage import feature
 import cv2
-
+import pymysql
+conn = pymysql.connect(host='localhost', user='root', password='', db='Primerjalko')
+db = conn.cursor()
 
 class LocalBinaryPatterns:
     def __init__(self, numPoints, radius):
@@ -16,50 +18,66 @@ class LocalBinaryPatterns:
     def describe(self, image, eps=1e-7):
         lbp = feature.local_binary_pattern(image, self.numPoints,
                                            self.radius, method="uniform")
-        (hist, _) = np.histogram(lbp.ravel(),bins=np.arange(0, 256),range=(0, 256))
+        (hist, _) = np.histogram(lbp.ravel(), bins=np.arange(0, 256), range=(0, 256))
 
         hist = hist.astype("float")
-        #hist /= (hist.sum() + eps)
+        # hist /= (hist.sum() + eps)
         cv2.normalize(hist, hist, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
         return hist
 
 
+my_path = os.path.abspath(os.path.dirname(__file__))
+print(my_path)
+path = os.path.join(my_path, "/saveImgToLocal/")
+path = my_path + '\saveImgToLocal\\'
+print(path)
 
-
-
-
-onlyfiles = [f for f in listdir("./SaveImgToLocal/") if isfile(join("./SaveImgToLocal/", f))]
+onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
 print(onlyfiles)
 
 desc = LocalBinaryPatterns(253, 8)
-data = [] #LBP histogram
-labels = [] #imena datotek
-HOGs = [] #HOG vrednosti
+data = []  # LBP histogram
+labels = []  # imena datotek
+HOGs = []  # HOG vrednosti
 for imagePath in onlyfiles:
-    ext=imagePath.split(".")
-    if(ext[1] == "jpg" or ext[1] == "png" or ext[1] == "bmp" or ext[1] == "jpeg" or ext[1] == "JPG"):
-        print("./SaveImgToLocal/" + imagePath)
-        image = cv2.imread("./SaveImgToLocal/" + imagePath)
+    ext = imagePath.split(".")
+    if ext[1] == "jpg" or ext[1] == "png" or ext[1] == "bmp" or ext[1] == "jpeg" or ext[1] == "JPG":
+        image = cv2.imread(path + imagePath)
         image = np.float32(image)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         gray = np.float32(gray)
         hist = desc.describe(gray)
         H = feature.hog(image, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), transform_sqrt=True)
         HOGs.append(H)
-        labels.append(imagePath)
+        imgName = imagePath.split(".")
+        labels.append(imgName[0])
         data.append(hist)
     else:
         continue
 
-prviDrugi = cv2.compareHist(np.float32(data[0]), np.float32(data[1]), cv2.HISTCMP_BHATTACHARYYA)
-print(prviDrugi)
-
 i = 0
-j = 0
-size = len(data)
-lenOfitem = len(data[0])
-print(lenOfitem)
-lenOfitem=len(data[1])
-print(lenOfitem)
+j = 1
 
-input('Press ENTER to exit')
+
+
+while i < len(data):
+    ime = labels[i]
+    set = data[i]
+    while j < len(data):
+        ime2 = labels[j]
+        set2 = data[j]
+        eqRate = cv2.compareHist(np.float32(set), np.float32(set2), cv2.HISTCMP_BHATTACHARYYA)
+        if eqRate < 0.15:
+            sql = "INSERT INTO similarproducts(Text, Products_ID, SimilarProduct_ID) VALUES ((%d), (%s), (%s))"%(eqRate, ime, ime2)
+            print(sql)
+            retVal = db.execute(sql)
+            conn.commit()
+            if retVal != 1:
+                print("Failed Insert")
+            else:
+                print("Sucess")
+        else:
+            print("not enough similiarity")
+        j = j + 1
+    i = i + 1
+conn.close()
