9dc93e5fad3ecfb835f818865b10724129e39306
Ivan Tomić <ivan.tomic17@gmail.com>
2019-06-04 12:35:42 +0200

    C:/Program Files/Git/

--- mlacom.php
+++ mlacom.php
@@ -4,27 +4,23 @@ include "db.php";
 /* 
  * KATEGORIJA: prenosniki
  */
-
 $query = mysqli_query($conn, "SELECT ID, StoreURL FROM Stores WHERE StoreURL = 'https://www.mlacom.si'");
-    $store_id = 0;
-        if(mysqli_num_rows($query) == 0)
-        {
-            $query1 = mysqli_query($conn, "INSERT into Stores(Name, StoreURL) VALUES('mlacom', 'https://www.mlacom.si')");
-            $query = mysqli_query($conn, "SELECT ID, StoreURL FROM Stores WHERE StoreURL = 'https://www.mlacom.si'");
-            $row = mysqli_fetch_assoc($query);
-            $store_id = $row['ID'];
-        }
-        else 
-        {
-            $row = mysqli_fetch_assoc($query);
-            $store_id = $row['ID'];
-        }
-
+$store_id = 0;
+if(mysqli_num_rows($query) == 0)
+{
+    $query1 = mysqli_query($conn, "INSERT into Stores(Name, StoreURL) VALUES('mlacom', 'https://www.mlacom.si')");
+    $query = mysqli_query($conn, "SELECT ID, StoreURL FROM Stores WHERE StoreURL = 'https://www.mlacom.si'");
+    $row = mysqli_fetch_assoc($query);
+    $store_id = $row['ID'];
+}
+else
+{
+    $row = mysqli_fetch_assoc($query);
+    $store_id = $row['ID'];
+}
 for ($i = 1; $i < 40; $i++) {
-
     $html = file_get_html('https://www.mlacom.si/prenosniki-in-oprema/prenosniki?p=' . $i);
     $velHtml = str_word_count($html, 0);
-
     if ($i == 1) { //pride skoz vse podstrani
         $vel = $velHtml;
     } else {
@@ -32,15 +28,13 @@ for ($i = 1; $i < 40; $i++) {
             break;
         }
     }
-
     foreach ($html->find('div.box') as $element) {
         $link = 'https://www.mlacom.si/';
         $item[] = $element->find('h2 a', 0)->plaintext; //title
         $title = $element->find('h2 a', 0)->plaintext;
-        $item[] = $element->find('p', 0)->plaintext; //description 
+        $item[] = $element->find('p', 0)->plaintext; //description
         $description = $element->find('p', 0)->plaintext;
         $price = $element->find('div.additional div', 0)->plaintext; //price
-
         $price = str_replace("Spletna cena:", "", $price);
         $price = str_replace("€", "", $price);
         $price = str_replace(".", "", $price);
@@ -48,16 +42,14 @@ for ($i = 1; $i < 40; $i++) {
         echo $price;
         $linkDesc = $element->find('a', 0)->href; //url
         $linkInsert = $link . $linkDesc; //url
-
         $image = $element->find('center img', 0)->src; //picture
         $image1 = str_replace("crop2/", "", $image);
         $img = $link . $image1;
-        
+
         $date = date("Y-m-d H:i:s");
         $query = mysqli_query($conn, "INSERT INTO Products(Title, ProductURL, Price, DateTime, Description,Rating, Stores_ID, Categories_ID) VALUES('$title', '$linkInsert', $price, '$date', '$description', 0, $store_id, (SELECT ID FROM Categories WHERE Title = 'Prenosniki'))");
         $query2 = mysqli_query($conn, "INSERT INTO Pictures(url, Title, Products_ID) VALUES('$img', '$title', (SELECT ID FROM Products WHERE ProductURL = '$linkInsert'))");
-        
+
     }
 }
-
 ?>
